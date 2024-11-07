d99c8731496f0ddd72fb18b108765b9ffa7aa66c
anerhernandez <anerhernandezl@gmail.com>
2024-11-07 17:46:05 +0000

    Creación de indice con descripción de la tarea

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Debemos crear una app web personal donde podamos llevar el registro de las calificaciones del alumnado. La app debe tener en cuenta:
+
+Tendremos 1 solo profesor (el usuario) y alumnos.
+Tendremos asignaturas (ej. DSW, DPL...).
+Los alumnos estarán matriculados en asignaturas.
+Habrán actividades y unidades (temas). La actividad pertenece a un tema y el tema pertenece a una asignatura.
+El usuario puede crear: asignaturas, alumnos, temas y actividades.
+Las calificaciones se asociarán a un alumno y a una actividad.
+Las calificaciones finales para cada alumno se calcularan con la media de los temas. Cada tema se calculará con la media de las actividades.
+La aplicación web debe ser lo más sencilla posible pero ser agil para el usuario. (por ej: que permita añadir varias calificaciones de una actividad para todos los alumnos a la vez)
+*/ 
+
+?>
